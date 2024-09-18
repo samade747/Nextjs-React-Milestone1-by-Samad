8ae05b6cce72ead6b0588d94a2a10366461af002
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-09-18 10:22:31 +0500

    .

--- samadmilestonenextjs1/src/app/page.tsx
+++ samadmilestonenextjs1/src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Main } from "next/document";
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-    
-    
-    </>  
+    <div className={StyleSheet.helloWorld}>
+        Hello world
+    </div>  
   );
 }
